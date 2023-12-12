738447eca2d1ea1083d8c1ec342f750fe4e7e986
SebastianTylicki <115147343+SebastianTylicki@users.noreply.github.com>
2023-12-12 18:31:55 +0100

    12.12.2023

--- WinFormsApp1/WinFormsApp1/Form1.Designer.cs
+++ WinFormsApp1/WinFormsApp1/Form1.Designer.cs
@@ -28,174 +28,71 @@
         /// </summary>
         private void InitializeComponent()
         {
-            textBox1 = new TextBox();
-            textBox2 = new TextBox();
-            label1 = new Label();
-            label2 = new Label();
-            checkBox1 = new CheckBox();
-            numericUpDown1 = new NumericUpDown();
-            button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
-            button4 = new Button();
-            button5 = new Button();
-            button6 = new Button();
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).BeginInit();
-            SuspendLayout();
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(131, 83);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(100, 23);
-            textBox1.TabIndex = 0;
-            textBox1.TextChanged += textBox1_TextChanged;
-            // 
-            // textBox2
-            // 
-            textBox2.Location = new Point(131, 161);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(100, 23);
-            textBox2.TabIndex = 1;
-            textBox2.TextChanged += textBox2_TextChanged;
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(447, 196);
-            label1.Name = "label1";
-            label1.Size = new Size(31, 15);
-            label1.TabIndex = 2;
-            label1.Text = "Czas";
-            label1.Click += label1_Click;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(537, 196);
-            label2.Name = "label2";
-            label2.Size = new Size(38, 15);
-            label2.TabIndex = 3;
-            label2.Text = "label2";
-            label2.Click += label2_Click;
-            // 
-            // checkBox1
-            // 
-            checkBox1.AutoSize = true;
-            checkBox1.Location = new Point(379, 86);
-            checkBox1.Name = "checkBox1";
-            checkBox1.Size = new Size(59, 19);
-            checkBox1.TabIndex = 4;
-            checkBox1.Text = "prawo";
-            checkBox1.UseVisualStyleBackColor = true;
-            checkBox1.CheckedChanged += checkBox1_CheckedChanged;
-            // 
-            // numericUpDown1
-            // 
-            numericUpDown1.Location = new Point(537, 83);
-            numericUpDown1.Name = "numericUpDown1";
-            numericUpDown1.Size = new Size(120, 23);
-            numericUpDown1.TabIndex = 5;
-            numericUpDown1.ValueChanged += numericUpDown1_ValueChanged;
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(543, 131);
-            button1.Name = "button1";
-            button1.Size = new Size(75, 23);
-            button1.TabIndex = 6;
-            button1.Text = "Generuj";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += button1_Click;
+            this.button1.Location = new System.Drawing.Point(94, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
-            button2.Location = new Point(136, 288);
-            button2.Name = "button2";
-            button2.Size = new Size(75, 23);
-            button2.TabIndex = 7;
-            button2.Text = "SB";
-            button2.UseVisualStyleBackColor = true;
-            button2.Click += button2_Click;
+            this.button2.Location = new System.Drawing.Point(211, 42);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
             // 
             // button3
             // 
-            button3.Location = new Point(248, 288);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 8;
-            button3.Text = "SS";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += button3_Click;
+            this.button3.Location = new System.Drawing.Point(343, 42);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // button4
+            // textBox1
             // 
-            button4.Location = new Point(363, 288);
-            button4.Name = "button4";
-            button4.Size = new Size(75, 23);
-            button4.TabIndex = 9;
-            button4.Text = "SI";
-            button4.UseVisualStyleBackColor = true;
-            button4.Click += button4_Click;
-            // 
-            // button5
-            // 
-            button5.Location = new Point(478, 288);
-            button5.Name = "button5";
-            button5.Size = new Size(75, 23);
-            button5.TabIndex = 10;
-            button5.Text = "SM";
-            button5.UseVisualStyleBackColor = true;
-            button5.Click += button5_Click;
-            // 
-            // button6
-            // 
-            button6.Location = new Point(582, 288);
-            button6.Name = "button6";
-            button6.Size = new Size(75, 23);
-            button6.TabIndex = 11;
-            button6.Text = "SQ";
-            button6.UseVisualStyleBackColor = true;
-            button6.Click += button6_Click;
+            this.textBox1.Location = new System.Drawing.Point(94, 92);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 23);
+            this.textBox1.TabIndex = 3;
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button6);
-            Controls.Add(button5);
-            Controls.Add(button4);
-            Controls.Add(button3);
-            Controls.Add(button2);
-            Controls.Add(button1);
-            Controls.Add(numericUpDown1);
-            Controls.Add(checkBox1);
-            Controls.Add(label2);
-            Controls.Add(label1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
-            Name = "Form1";
-            Text = "Form1";
-            Load += Form1_Load;
-            ((System.ComponentModel.ISupportInitialize)numericUpDown1).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
 
-        private TextBox textBox1;
-        private TextBox textBox2;
-        private Label label1;
-        private Label label2;
-        private CheckBox checkBox1;
-        private NumericUpDown numericUpDown1;
         private Button button1;
         private Button button2;
         private Button button3;
-        private Button button4;
-        private Button button5;
-        private Button button6;
+        private TextBox textBox1;
     }
 }
--- WinFormsApp1/WinFormsApp1/Form1.cs
+++ WinFormsApp1/WinFormsApp1/Form1.cs
@@ -1,124 +1,150 @@
-using System.Diagnostics;
-
 namespace WinFormsApp1
+
 {
     public partial class Form1 : Form
     {
+        private DrzewoBinarne d;
+
         public Form1()
         {
             InitializeComponent();
+            
         }
-
         private void Form1_Load(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+        private void button1_Click_1(object sender, EventArgs e)
         {
 
         }
 
-        private void textBox2_TextChanged(object sender, EventArgs e)
+        private void button2_Click_1(object sender, EventArgs e)
         {
-
-        }
-
-        private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-
+            
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
+            d = new DrzewoBinarne(5);
+            d.Add(8);
+            d.Add(4);
+            d.Add(2);
+            d.Add(3);
+            d.Add(1);
 
         }
+    }
 
-        private void button4_Click(object sender, EventArgs e)
+    public class Wezel3
+    {
+        public int wartosc;
+        public Wezel3 rodzic;
+        public Wezel3 lewedziecko;
+        public Wezel3 prawedziecko;
+
+        public Wezel3(int liczba)
         {
-
+            this.wartosc = liczba;
         }
 
-        private void button5_Click(object sender, EventArgs e)
+        public void Add(Wezel3 w)
         {
-
-        }
-
-        private void button6_Click(object sender, EventArgs e)
-        {
-
-        }
-        private void czas()
-        {
-            Stopwatch stopwatch = new Stopwatch();
-            stopwatch.Start();
-
-            int n = (int)numericUpDown1.Value;
-            int[] tablica = new int[n];
-
-            stopwatch.Stop();
-            Console.WriteLine("Czas generowania tablicy: {0} sekund", stopwatch.Elapsed.TotalSeconds);
-        }
-        private void SS(int[] arr)
-        {
-            int n = arr.Length;
-            for (int i = 0; i < n - 1; i++)
+            if (w.wartosc < this.wartosc)
             {
-                int minIndex = i;
-                for (int j = i + 1; j < n; j++)
+                if (this.lewedziecko == null)
                 {
-                    if (arr[j] < arr[minIndex])
+                    this.lewedziecko = w;
+                    w.rodzic = this;
+                }
+                else
+                {
+                    this.lewedziecko.Add(w);
+                }
+            }
+            else
+            {
+                if (this.prawedziecko == null)
+                {
+                    this.prawedziecko = w;
+                    w.rodzic = this;
+                }
+                else
+                {
+                    this.prawedziecko.Add(w);
+                }
+            }
+        }
+
+        public void Add(int liczba)
+        {
+            var dziecko = new Wezel3(liczba);
+            dziecko.rodzic = this;
+            if (liczba < this.wartosc)
+            {
+                this.lewedziecko = dziecko;
+            }
+            else
+            {
+                this.prawedziecko = dziecko;
+            }
+        }
+
+        public override string ToString()
+        {
+            return wartosc.ToString();
+        }
+        
+
+    }
+
+
+
+    public class DrzewoBinarne
+    {
+        private Wezel3 korzen;
+
+        public DrzewoBinarne(int liczba)
+        {
+            this.korzen = new Wezel3(liczba);
+        }
+
+        public void Add(int liczba)
+        {
+            var rodzic = this.ZnajdzWezel(liczba);
+            rodzic.Add(liczba);
+        }
+
+
+        private Wezel3 ZnajdzWezel(int liczba)
+        {
+            var w = this.korzen;
+            while (true)
+            {
+                if (liczba < w.wartosc)
+                {
+                    if (w.lewedziecko == null)
                     {
-                        minIndex = j;
+                        return w;
+                    }
+                    else
+                    {
+                        w = w.lewedziecko;
                     }
                 }
-                int tmp = arr[minIndex];
-                arr[minIndex] = arr[i];
-                arr[i] = tmp;
-
-            }
-        }
-        private void SI(int[] arr)
-        {
-            int n = arr.Length;
-            for (int i = 1; i < n; ++i)
-            {
-                int key = arr[i];
-                int j = i - 1;
-
-                while (j >= 0 && arr[j] > key)
+                else
                 {
-                    arr[j + 1] = arr[j];
-                    j = j - 1;
+                    if (w.prawedziecko == null)
+                    {
+                        return w;
+                    }
+                    else
+                    {
+                        w = w.prawedziecko;
+                    }
                 }
-                arr[j + 1] = key;
             }
         }
-
     }
 }
